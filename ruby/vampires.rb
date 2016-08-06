9d7f0e1989ac1c03b70846fbfec99a67fe830f94
2k$ <kamronr3@gmail.com>
2016-08-06 13:16:43 -0700

    finshed asgn 4.4. add allgeries plot twist and fixe logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,37 +3,58 @@ puts "how many employees do you want to do this for?"
 employees = gets.chomp
 employees = employees.to_i
 until  employees ==  0
+	employees = employees - 1
 puts "what is your name?"
 	name = gets.chomp
-	if name = "Drake Cula" || "Tu Fang"
+	if name == "Tu Fang"
 		sure = true
+	elsif name == "Drake Cula"
+		sure = true	
+	else
+		true
 	end
 puts "how old are you?"
 		old = gets.chomp
 		old = old.to_i
 			if old <= 100
 		age = true
+	else
+		age = false
 	end
 puts "our company servers garlic bread... should we order some for you?"
 		garlic = gets.chomp
 		if garlic == "yes"
 		food = true
+	else
+		false
 	end
 puts "Would you like to enrol in the company's health plan?"
 	insurance = gets.chomp
 		if insurance == "yes"
 		plan = true
-	end
-if (age)  && (food || plan) && (!sure)
-	puts "probably not a vampire"
-	elsif(!age)  && (food ||plan)
-	puts "probably a vampire"
-	elsif (!age) && (!food) && (!plan) 
-		puts "almost certainly a vampire"	
-	elsif ((age) && (food) &&(plan)) && (sure) 
-		puts "Definetely a vampire"
 	else
-		puts "results inconclusive"
+		plan = false
 	end
-		employees = employees- 1
+	puts "list any allergies"
+	allergies = nil
+	until  (allergies =="done") || (allergies == "sunshine")
+		puts "type done when finished"
+		allergies = gets.chomp
 	end
+vampire_data = nil
+if allergies == "sunshine" 
+	vampire_data = "probably a vampire"
+	elsif  sure == true
+	vampire_data = "definitely a vampire"
+	elsif (age && food) || (age && plan)
+	vampire_data = "probably a vampire data"		
+	elsif (!age) && (!food) && (!plan) 
+		vampire_data = "almost certainly a vampire"	 			 		
+	else
+		vampire_data = "results inconclusive"	
+	end
+	
+puts vampire_data	
+end
+
+	puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
