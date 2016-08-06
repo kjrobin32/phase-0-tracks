d740c1cfd1e342d3d1eaf8525059f5e54d1a5d80
2k$ <kamronr3@gmail.com>
2016-08-06 09:03:23 -0700

    complete vampires with release 3.2 and start on loop for 3.3

---
+++ ruby/vampires.rb
@@ -0,0 +1,41 @@
+
+
+
+puts "how many employees do you want to do this for?"
+employees = gets.chomp
+employees = employees.to_i
+employees = n 
+while  employees >  n
+puts "what is your name?"
+	name = gets.chomp
+	if name = "Drake Cula" || "Tu Fang"
+		sure = true
+	end
+puts "how old are you?"
+		old = gets.chomp
+		old = old.to_i
+			if old <= 100
+		age = true
+	end
+puts "our company servers garlic bread... should we order some for you?"
+		garlic = gets.chomp
+		if garlic == "yes"
+		food = true
+	end
+puts "Would you like to enrol in the company's health plan?"
+	insurance = gets.chomp
+		if insurance == "yes"
+		plan = true
+	end
+if (age)  && (food || plan) && (!sure)
+	puts "probably not a vampire"
+	elsif(!age)  && (food ||plan)
+	puts "probably a vampire"
+	elsif (!age) && (!food) && (!plan) 
+		puts "almost certainly a vampire"	
+	elsif ((age) && (food) &&(plan)) && (sure) 
+		puts "Definetely a vampire"
+	else
+		puts "results inconclusive"
+		employees +=1
+end
