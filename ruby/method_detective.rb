f933d955a6d6e9725ca685ab96566591d50ff1de
2k$ <kamronr3@gmail.com>
2016-08-11 05:23:37 -0700

    add method-dectctive and finish 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase!
+=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+"zom".insert(1,"o")
+=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+ "enhance".center(15)
+ => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+ "Stop! You’re under arrest!".upcase
+ => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+"the usual".ljust(17," suspects")
+=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+" suspects".rjust(19,"the ususal")
+ => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chomp("r")
+=> "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+"The mystery of the missing first letter"slice(1,39)
+=> "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+"Elementary,    my   dear        Watson!".split.join(" ")
+=> "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+"z".ord
+=> 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string?".count("a")
+ => 4
