7010d97bd5677a0f43d314c9019a52917f17fb70
2k$ <kamronr3@gmail.com>
2016-08-20 20:20:28 -0700

    update solo.rb with intialize and school def.

---
+++ ruby/solo.rb
@@ -0,0 +1,24 @@
+class Soocer_ball
+	attr_accessor :generation, :size, :field
+	def initialize (generation, size, field)
+	@generation = generation
+	@size = size
+	@field = field
+	end
+	def school(generation)
+	puts "what generation is your ball"
+	generation = gets.chomp
+	@generation = generation
+	ball = false
+	puts "@generation"
+		until 	generation == ("old school" || "new school" || modern) 
+		puts "try agian!"
+		ball = 
+	end
+	end
+end
+
+# A soocer ball can be old school, new school, and modern , between 1-5, and be meant for artifical or grass turf.
+
+nball = Soocer_ball.new("old school", 5, "turf")
+nball.school("old school")
