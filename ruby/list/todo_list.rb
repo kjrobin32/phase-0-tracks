3903b42e33301aff766df1a2b68f8646aee7261f
2k$ <kamronr3@gmail.com>
2016-08-22 06:38:39 -0700

    add method for rspec.

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -5,5 +5,14 @@ class TodoList
 	end
 	def get_items
 		@list_ary
-	end 	
+	end
+	def add_item(item)
+		@list_ary << item
+	end
+	def delete_item(item)
+		@list_ary.delete(item)
+	end
+	def get_item(index)
+		@list_ary[index]
+	end
 end
