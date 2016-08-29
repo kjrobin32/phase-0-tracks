616c34b650ef0dc3e4d13f6e5a7f1e73e3298d9e
2k$ <kamronr3@gmail.com>
2016-08-28 22:26:43 -0700

    delete unnecesary code from file.

--- js/explore.js
+++ js/explore.js
@@ -11,13 +11,3 @@ return revWord
 console.log(reverse("hello"))
 
 
-var isOver21 = true;
-var drinksAlcohol = true;
-
-if (isOver21 && drinksAlcohol) {
-	console.log("Would you like a drink -- beer or wine, perhaps?");
-} else if (isOver21 && !drinksAlcohol) {
-	console.log("Can I get you an ice water, coffee, tea, or soft drink?");
-} else {
-	console.log("One Shirley Temple, coming up!");
-}
