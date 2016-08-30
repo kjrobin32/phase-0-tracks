f30f5b8213d49c3b8dabe2074db40b20cb9f1bcc
2k$ <kamronr3@gmail.com>
2016-08-30 05:01:40 -0700

    add getrandomint and random word function.

--- js/algos.js
+++ js/algos.js
@@ -5,9 +5,6 @@
 // reutrn the values of the longets array 
 //  
 
-
-
-
 // function compare(){
 // 	var phrases = [];
 // 	longWord = ""
@@ -24,23 +21,18 @@
 // compare()
 
 
-function compare(){
+function compare(longWord){
 	var phrases = [];
 	var longWord = "";
-	(phrases.push('bighat', 'smallcat', 'snack'));
 		for (var i = 0; i < phrases.length; i++) {
 			if (phrases[i].length > longWord.length) {
 				longWord = phrases[i]
 			}
-			// for (k = i + 1; k < phrases.length; k++)  {
-			// 	if (phrases[i] != phrases[k]){
-									
-			// 						}
-			// }
-		}	
-	return console.log(longWord)
+			phrases.push(longWord)
+		}
+	return phrases
 }
-compare()
+console.log(compare('bighat', 'smallcat', 'snack'))
 
 // console.log(phrases[''].length)
 // phrases[i].length;
@@ -52,3 +44,60 @@ compare()
 
 // 	}
 // console.log(compare.length);
+
+
+// release #1
+// 
+
+// input at 2 objeccts into a method. 
+function checker(obj1, obj2){
+
+var value = false;
+// The function will compare each key in value for each object
+// 
+for (var i = 0; i < Object.keys(obj1).length; i++) {
+	if (Object.keys(obj1)[i] == Object.keys(obj2)[i]) {
+	value = true	
+	}
+return value
+}
+
+// if one of the key and vlaue match (or_) the objects. 
+
+// return true
+}
+var obj1 = {name: "Steven", age: 54}
+var obj2 = {name: "Tamir", age: 54}
+console.log(checker(obj1, obj2))
+
+
+//I tried to use Objects.values but it does not exist yet
+// && (Object.values(obj1)[i] == Object.values(obj2)[i]))
+
+
+// a function that takes a number. The number will tell how many words to create from a list of letters.  The words will have random letters mcut have a min of 1 and max of 10
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+function randomWords(number){	
+	var letters = "abcdefghijklmnopqrstuvwxyz";
+	var words = [];
+	for (var i = 0; i < number + 1; i++) {
+		var newWord = "";
+		var length = getRandomInt(1,11);
+		for (var x = 0; x < length; x++) {
+			newWord += letters[getRandomInt(0,26)];			
+		}
+	words.push(newWord);
+	}
+	return words
+}
+	
+console.log(randomWords(7))
+
+
