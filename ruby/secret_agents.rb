c96dabc1066e3ef817151457b568c24a6673c35b
2k$ <kamronr3@gmail.com>
2016-09-19 08:15:37 -0700

    add decrypt and encrypt until statement. add agent insturctions

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,49 +1,74 @@
 =begin
 starting with the first letter
-until all the letters in the word have been reviewed
+until all the letters in the password have been reviewed
 	if the letter is z it will need to be change to a eveything else will need to be moved to the next letter.
 =end
 
-def encrypt(word)
+def encrypt(password)
 index = 0
-	until index == word.length
-	if word[index] == "z"
-		word[index]= "a"
-	elsif word[index] == " "
-			word[index] = " "			
+	until index == password.length
+	if password[index] == "z"
+		password[index]= "a"
+	elsif password[index] == " "
+			password[index] = " "			
 	else 
-		word[index] = word[index].next	
+		password[index] = password[index].next	
 	end
 	index = index + 1
 	end
-	return word 
+	return password 
 end
 
-word = gets.chomp
-puts encrypt(word)
+# password = gets.chomp
+# puts encrypt(password)
 
 =begin
 sarting with the first lettet
 install all the letters have been reviewed
-set the index letter of the word = 
+if there is a space leave it as a space
+set the index letter of the password equal to (-1) the index of the  the alphabet
 	
 =end
-
-def decrypt(word)
+# Hello agent.  The purpose of the program is into encrypt and decypt your password. By putting in your passcode, the program will encrypty your password or decrypt it for your mission. 
+def decrypt(password)
 index = 0
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-	until index == word.length 
-		if word[index] == " "
-			word[index] = " "
+	until index == password.length 
+	if password[index] == " "
+			password[index] = " "
 	else 
-		word[index]  = alphabet[alphabet.index(word[index])-1]
-end
+		password[index]  = alphabet[alphabet.index(password[index])-1]
+	end
 	index = index + 1
 	end
-	return word 
+	return password 
 end
 
-puts decrypt(word)
+puts "Do you want to encrypt or decrypt your password?"
 
-puts decrypt(encrypt(word))
+crypt_choice = false
+until crypt_choice == true
+	user_input = gets.chomp
+	if user_input == "encrypt"
+		puts "Input password to encrypt"
+		password = gets.chomp
+		puts encrypt(password)	
+		crypt_choice = true
+	elsif user_input == "decrypt"
+		puts "Input password to decrypt"
+		password = gets.chomp
+		puts decrypt(password)			
+		crypt_choice = true
+	else
+		puts "Please enter decrypt to encrypt!"
+		crypt_choice = false
+		
+	end
+ end
+
+# puts decrypt(password)
+
+
+
+# puts decrypt(encrypt(password))
 
