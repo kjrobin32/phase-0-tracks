66182c924987c67bdaec9288ad39aa64bc17f66c
2k$ <kamronr3@gmail.com>
2016-09-28 14:49:29 -0700

    add gsub puts statement to agent and make name variable to a string.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,4 +1,5 @@
 
+=begin
 puts "Agent you are in a whole lot of crap and need to get to the choooppa! but before you can get to the choppa, you need a new alias....!!!"
 name = nil
 until name == "quit"
@@ -34,5 +35,31 @@ end
 
 #with with uaeio repectively
 
+=end
+=begin
+	
+secondtime 
+=end
 
-end
+=begin
+get real name
+swap the first array with the second array
+split the letters of array
+if the letter is big and a vawoel move it over 1 big value
+	if the letter us lower and a value make it 
+if the letter is gib and a consotant make it to thenext constant
+	iff if it is small the make it inttthe next small xonanant
+
+=end
+
+
+name = "Kamron Robinson"
+puts "Agent you are in a whole lot of crap and need to get to the choooppa! but before you can get to the choppa, you need a new alias....!!! Please input your first and lasy name."
+name = name.split.reverse
+name = name.to_s
+
+name.gsub!(/[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]/, 'a' => 'e', 'A' => 'E', 'e' => 'i', 'E' => 'I', 'i' => 'o', 'I' => 'O' 'O' => 'U',"o" => 'u,' 'u' => 'a', 'U' => 'A', 'b' => 'c', 'c' => 'd', 'd' => 'f', 'f' => 'g', 'g' => 'h', 'h' => 'j', 'j' => 'k', 'k' => 'l', 'l' => 'm', 'm' => 'n', 'n' => 'p', 'p' => 'q', 'q' => 'r', 'r' => 's', 's' => 't', 't' => 'v', 'v' => 'w', 'w' => 'x', 'x' => 'y', 'y' => 'z', 'z' => 'b', 'B' => 'C', 'C' => 'D', 'D' => 'F', 'F' => 'G', 'G' => 'H', 'H' => 'J', 'J' => 'K', 'K' => 'L', 'L' => 'M', 'M'=>'N', 'N' => 'P', 'P' => 'Q', 'Q' => 'R', 'R' => 'S', 'S' =>'T', 'T'=> 'V', 'V' => 'W', 'W' => 'X', 'X' => 'Y', 'Y' => 'Z')
+
+
+puts name
+p name.class
