7967a3135db37fb21e98850e19f51f863359264f
2k$ <kamronr3@gmail.com>
2016-09-30 13:19:56 -0700

    add method bublle sorting method and pseudo code.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -16,16 +16,53 @@ then takes the number and puts it in the array
 
 # p 100.times {|x| numbers_array[x] + numbers_array[x + 1]}
 
-def fil(i)
-	numbers_array = [0, 1]
-	i = 0
-	while i < 100-1	
-	 new_number = numbers_array[i] + numbers_array[i + 1]
-	 numbers_array << new_number
-	 i += 1
-	end
-	puts numbers_array[i]
+# def fil(i)
+# 	numbers_array = [0, 1]
+# 	i = 0
+# 	while i < 100-1,	
+# 	 new_number = numbers_array[i] + numbers_array[i + 1]
+# 	 numbers_array << new_number
+# 	 i += 1
+# 	end
+# 	puts numbers_array[i]
+# end
+
+# puts fil(100)
+
+
+
+array = [1, 24, 100, 69, 20 , 50]
+
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+  puts array
 end
 
-puts fil(100)
+puts bubble_sort(array)
 
+=begin
+
+The bubble array is a long method to orginize numbers in an array from lowest to highhest.	
+
+pseudocode:
+
+create a method
+that takes an array
+and moves down each number
+if the number in the array is larger than then next index
+	swap the numbers
+	otherwise
+	do not swap
+=end
