2864fdde58951d89bdc98ce5e4b6aecb84fb2a43
2k$ <kamronr3@gmail.com>
2016-10-07 07:13:19 -0700

    add two it statements for validate empty_word and guesses method.

--- ruby/guess/guess_spec.rb
+++ ruby/guess/guess_spec.rb
@@ -2,9 +2,20 @@ require_relative 'guess_game'
 describe GuessGame do
 	let(:game)	 { GuessGame.new("chat".chars) }
 
-it " creates two letter arrays that are equal to each other and one array" do
-expect(game.input("chat".chars)).to eq  ["_", "_", "_", "_"]
+it "takes the charachters in an array and makes an array with _ for each index" do
+expect(game.empty_word("chat".chars)).to eq  ["_", "_", "_", "_"]
 	end
+it "has limited guesses until 10" do 
+	expect(game.guesses(0, 4)).to eq 4
+	end
+
+
+
+
+# it " takes the letters entered and matches them to the first a" do
+# game.letter_checker(["_", "_", "_", "_"])
+# expect(game.input("chat".chars)).to eq  ["c", "h", "a", "t"]
+# 	end
 end
 
 # class GuessGame 
