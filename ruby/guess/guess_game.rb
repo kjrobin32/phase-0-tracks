d411e0c823616575828d282d5157e472875b527e
2k$ <kamronr3@gmail.com>
2016-10-08 22:43:12 -0700

    Add guess games class with attr reader attempt counter, initialize mothd, take letter method, run game method. update require method with it statments. 2 pass. update guess_2.rb.

--- ruby/guess/guess_game.rb
+++ ruby/guess/guess_game.rb
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 =begin
 please enter a word
 
@@ -21,96 +29,96 @@ new_word = gets.chomp
 word = new_word.chars
 holder = new_word.chars
 =end
-class GuessGame
-	attr_reader :letter
-	attr_reader :user_input
-	attr_reader :guesses
-	attr_reader :count
-def initialize(first_user_word)
-	@first_user_word = first_user_word
-	@second_user_word = first_user_word
-	# @user_input = first_user_word.fill("_")
-	@count = 0
-	@guesses = first_user_word.length	
-	end
-
-def empty_word(first_user_word)
-	@second_user_word = first_user_word.fill("_")
-	end
-
-# def guesses(count, guesses)			
-# 		until count > guesses
-# 		count += 1
-# 		return guesses	
-# 		end
-def letter_checker(second_user_word, letter)
-	until second_user_word == first_user_word
-	i = 0
-		while i < first_user_word.length			
-		if first_user_word[i] == letter			
-			second_user_word[i] = first_user_word[i]	
-			else			
-		end
-		i += 1
-		end					
-	end
-
-end
-puts "First player. please enter a word."
-first_user_word = gets.chomp
-game = GuessGame.new((first_user_word.chars))
-second_user_word = nil
-game.empty_word(first_user_word.chars)
-puts "enter a letter to guess the word."
-letter = gets.chomp	
-# if the oupt ot hit letter metts this criter is goes here
-
-	
-end
-
-# count = 0
-
-# guesses = first_user_word.length
-
-
-
-
-
-# p game.empty_word(first_user_word.chars)
-# puts "Please enter a letter for a guess?"
-
-
-# until letter != letter
-# letter = gets.chomp
-# guess_count = 0
-# 	if 
-# 	guess_count = letter.to_i
-# 		puts "try agin"
+# class GuessGame
+# 	attr_accessor :user_input, :holder
+# 	def initialize(holder)
+# 	@word = "chat".chars
+# 	@holder = "chat".chars	
+# 	@user_input = holder.fill("_")
+# 	end
+# 	def input(holder)
+# 	@holder = holder.fill("_")
 # 	end
 
+# 	def guess_count
+# 	@count += 1	
+# 	@guess =! @guess
+# 	end
+# 	def winner
+# 	@attempt == @word
+# 	"good job"
+# 	end	
+# end
 
 
 
 
-	# puts "#{user_input}"	
-
-# p gamer.guesses(count, guesses)
-
-
+# word = "chat".chars
+# holder = "chat".chars
+# user_input = holder.fill("_")
 
 
-	
+# p word = "chat".chars
+# p holder = "chat".chars
+# p user_input = holder.fill("_")
 
+# until user_input == word	
+# 	count = 0		
+# 		while count < word.length
+# 		puts "please enter a letter"
+# 		letter = gets.chomp	
+# 		i = 0
+# 		until i > word.length
+# 			if word[i] == letter			
+# 				user_input[i] = word[i]
+# 				p user_input	
+# 				else
+# 					puts "no"
+# 			end
+# 			i += 1	
+# 		end		
+# 	end
+# 	count += 1	
 
-
-
-
-# until user_input == word
-# 	puts "please enter a letter"
-# 	letter = gets.chomp	
-
+# 	puts "#{user_input}"	
 # end
 
 # puts "Good job!"
 
-# if user_input == word
+
+=begin
+user 1 inputs a word
+user 2 inputs a letter
+
+the word is then put as a blank array
+the letter compared to each letter in the word.
+if the letter matches the word the array is updated with the letter and put on the screen.
+ user only have 5 more guess than the length of the word.
+ when the word is achieve the user sees "good job"
+=end
+
+class GuessGame 
+	attr_reader :attempt_counter
+	def initialize(word)
+		@word = word
+		@attempt_counter = 0
+		@game_board = "_"*word.length
+		@guessed_letters = []
+	end
+	def take_letter(letter)
+		if @guessed_letters.index(letter) != nil
+			if @word.index(letter) != nil
+				@game_board[@word.index(letter)] = letter
+				@guessed_letters << letter	
+			end	
+			@attempt_counter += 1 
+		end
+	end
+	def run_game
+		until attempt_counter == word.length + 7
+			puts "Give me a letter"
+			letter = gets.chomp
+		end
+
+	end
+end
--- ruby/guess/guess_spec.rb
+++ ruby/guess/guess_spec.rb
@@ -1,22 +1,35 @@
 require_relative 'guess_game' 
 describe GuessGame do
-	let(:game)	 { GuessGame.new("chat".chars) }
+	let(:game)	 { GuessGame.new("word") }
 
-it "takes the charachters in an array and makes an array with _ for each index" do
-expect(game.empty_word("chat".chars)).to eq  ["_", "_", "_", "_"]
+	it "does not count the letter as an attempt if the letter has already been guessed. " do
+		game.take_letter"l"
+		expect{game.take_letter"l"}.not_to change {game.attempt_counter}
 	end
-it "has limited guesses until 10" do 
-	expect(game.guesses(0, 4)).to eq 4
+	it "has less attempts less than the length of the word" do
+		expect(game.attempts_count?(3)).to <  4
 	end
-
-
-
-
-# it " takes the letters entered and matches them to the first a" do
-# game.letter_checker(["_", "_", "_", "_"])
-# expect(game.input("chat".chars)).to eq  ["c", "h", "a", "t"]
-# 	end
+	it "if the guess is the same it does not count" do
+		expect(game.guess_count(10)).to eq 10
+	end
+	it "has less attempts than the length of the word" do
+		expect(game.guess_letter?(l)).to eq "L", "_", "E"
+	end
+	it " puts good job" do
+		expect (game.over(word)).to eq "Good Job"
+	end
 end
+
+
+
+# it "takes the charachters in an array and makes an array with _ for each index" do
+# expect(game.input("chat".chars)).to eq  ["_", "_", "_", "_"]
+# 	end
+# it " takes the letters entered and matches them to the first a" do
+# game.letter_checker(["c", "h", "a", "t"], ["_", "_", "_", "_"], ).to eq  ["c", "h", "a", "t"]
+# # expect(game.input("chat".chars.fill(''))).to ]
+# 	end
+# end
 
 # class GuessGame 
 # 	def initialize(holder)
